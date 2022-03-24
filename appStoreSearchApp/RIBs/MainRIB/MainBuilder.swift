b1bad2b0ab28eae0303d8bfad348b059def8261a
chan614 <pchan5076@gmail.com>
2022-03-24 17:03:32 +0900

    주석제거

--- appStoreSearchApp/RIBs/MainRIB/MainBuilder.swift
+++ appStoreSearchApp/RIBs/MainRIB/MainBuilder.swift
@@ -9,13 +9,11 @@ import RIBs
 import Foundation
 
 protocol MainDependency: Dependency {
-    // TODO: Declare the set of dependencies required by this RIB, but cannot be
-    // created by this RIB.
+    
 }
 
 final class MainComponent: Component<MainDependency> {
-
-    // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
+    
 }
 
 // MARK: - Builder
--- appStoreSearchApp/RIBs/MainRIB/MainViewController.swift
+++ appStoreSearchApp/RIBs/MainRIB/MainViewController.swift
@@ -10,9 +10,7 @@ import RxSwift
 import UIKit
 
 protocol MainPresentableListener: AnyObject {
-    // TODO: Declare properties and methods that the view controller can invoke to perform
-    // business logic, such as signIn(). This protocol is implemented by the corresponding
-    // interactor class.
+    
 }
 
 class MainViewController: UITabBarController, MainPresentable, MainViewControllable {
@@ -37,19 +35,6 @@ class MainViewController: UITabBarController, MainPresentable, MainViewControlla
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
